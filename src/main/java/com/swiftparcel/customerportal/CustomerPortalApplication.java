aff3ee030c07b48de2634964ec7fc101401e1242
EdCenten0 <cchavarriacenteno8@gmail.com>
2026-07-24 14:09:37 +0200

    chore(config): added JWT properties for testing, and enabled configuration scan of the main class for reading the yaml file

--- src/main/java/com/swiftparcel/customerportal/CustomerPortalApplication.java
+++ src/main/java/com/swiftparcel/customerportal/CustomerPortalApplication.java
@@ -2,8 +2,10 @@ package com.swiftparcel.customerportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CustomerPortalApplication {
 
 	public static void main(String[] args) {
