9adbfa77c54fdd0a4530693e77f71b98c4cdd5dc
Denilson <denilson.s@tcs.com>
2026-08-01 10:52:05 +0200

    Remove unused imports from

--- src/main/java/com/swiftparcel/customerportal/controller/PricingController.java
+++ src/main/java/com/swiftparcel/customerportal/controller/PricingController.java
@@ -2,12 +2,8 @@ package com.swiftparcel.customerportal.controller;
 
 import com.swiftparcel.customerportal.dto.PricingDTO;
 import com.swiftparcel.customerportal.dto.PricingRequestDTO;
-import com.swiftparcel.customerportal.model.Address;
-import com.swiftparcel.customerportal.model.enums.ServiceType;
 import com.swiftparcel.customerportal.service.PricingService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
