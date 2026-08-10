698a90b42d3c0ed9a86447f41f8ee59ef1b67c0a
Carlos Chavarría Centeno <cchavarriacenteno8@gmail.com>
2026-08-10 11:16:19 +0200

    (fix): migration file name fixed

--- src/main/resources/db/migration/V4_4__pickup_request_fix_quote.sql
+++ src/main/resources/db/migration/V4_4__pickup_request_fix_quote.sql
@@ -2,3 +2,4 @@ ALTER TABLE pickup_request ADD COLUMN accepted_quote_id BIGINT;
 ALTER TABLE pickup_request DROP COLUMN quoted_price_eur;
 ALTER TABLE pickup_request DROP COLUMN quote_expires_at;
 ALTER TABLE pickup_request ALTER COLUMN status TYPE VARCHAR(32);
+ALTER TABLE pickup_request ADD COLUMN created_at TIMESTAMPTZ;
